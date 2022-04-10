55436a2c4dbeed3d6324d1db99886e4a4309cdde
Cavan Chung <75390863+CavanChung@users.noreply.github.com>
2022-04-10 23:00:44 +0000

    Finished

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,91 @@
+/**
+ * Name:Processing Task 6
+ * Purpose: Using methods to set multiple parameters whilist drawing objects
+ * Author: Cavan Chung
+ * Created:2022/04/10
+ */
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
   
-  // define other methods down here.
+  public void setup() {
+    background(255);
+  }
+
+  public void draw() {
+    //Enter dimensions/location to get a returned value of the center width
+    int intCenter = intCenterCalc(120, 120);
+
+    //Draw Level 4 Mark Flower, using the returned value (Location) 
+    intDrawFlower1(intCenter);
+    
+    //Draw Level 3 Mark Flower (Location, size, colour)
+    intDrawFlower2(50, 50, 200, 200, 225);
+
+    //Draw Level 2 Mark Flower
+    intDrawFlower3(120, 120);
+    
+    }  
+  
+
+
+  // Return the value of intWidth and intHeight
+  private int intCenterCalc(int intWidth, int intHeight) {
+
+    int intX = width/2;
+    return intX;
+    }  
+
+  // Set Flower 1 Parameters, (location using return)
+  private void intDrawFlower1(int intCenter) {
+
+    stroke(0);
+    translate(200, 200);
+    
+    for (int i = 0; i <= 8; i += 1) {
+      rotate(radians(45));
+      fill(0, 0, 0);
+      ellipse(0, 0, width/20, height/3);  
+    }
+      fill(0, 0, 0);
+      ellipse(0, 0, width/6, height/6);
+    }  
+  
+// Set Flower 2 Parameters, (location, size, and colour (black,white or gray))
+  private void intDrawFlower2(int intWidth, int intHeight, int intSizeX, int intSizeY, int intColour) {
+
+    stroke(0);
+    translate(intWidth, intHeight);
+    
+    for (int i = 0; i <= 8; i += 1) {
+      rotate(radians(45));
+      fill(intColour);
+      ellipse(0, 0, intSizeX/20, intSizeY/3);  
+    }
+      fill(intColour);
+      ellipse(0, 0, intSizeX/6, intSizeY/6);
+    }    
+
+
+  // Set Flower 3 Parameters, (location)
+  private void intDrawFlower3(int intTransX, int intTransY) {
+
+    stroke(0);
+    translate(intTransX / 2.1f + intTransX / 4.1f, intTransY / 2.1f + intTransY / 4.1f);
+    
+    for (int i = 0; i <= 8; i += 1) {
+      rotate(radians(45));
+      fill(0, 0, 0);
+      ellipse(0, 0, width/20, height/3);  
+    }
+      fill(0, 0, 0);
+      ellipse(0, 0, width/6, height/6);
+    }   
+  
 }
+
+
